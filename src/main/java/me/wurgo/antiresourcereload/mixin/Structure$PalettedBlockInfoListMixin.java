c65e4608a3d9208fa0f3b8f1fcbe70580434f9cd
contaria <kingcontaria@gmail.com>
2025-02-07 23:34:06 +0100

    fix: wrap field set instead of map instantiation

--- src/main/java/me/wurgo/antiresourcereload/mixin/Structure$PalettedBlockInfoListMixin.java
+++ src/main/java/me/wurgo/antiresourcereload/mixin/Structure$PalettedBlockInfoListMixin.java
@@ -1,7 +1,9 @@
 package me.wurgo.antiresourcereload.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.structure.Structure;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -11,14 +13,15 @@ import java.util.Map;
 @Mixin(Structure.PalettedBlockInfoList.class)
 public abstract class Structure$PalettedBlockInfoListMixin {
 
-    @ModifyExpressionValue(
+    @WrapOperation(
             method = "<init>*",
             at = @At(
-                    value = "INVOKE",
-                    target = "Lcom/google/common/collect/Maps;newHashMap()Ljava/util/HashMap;"
+                    value = "FIELD",
+                    target = "Lnet/minecraft/structure/Structure$PalettedBlockInfoList;blockToInfos:Ljava/util/Map;",
+                    opcode = Opcodes.PUTFIELD
             )
     )
-    private Map<?, ?> synchronizeBlockToInfosMap(Map<?, ?> blockToInfos) {
-        return Collections.synchronizedMap(blockToInfos);
+    private void synchronizeBlockToInfosMap(Structure.PalettedBlockInfoList list, Map<?, ?> blockToInfos, Operation<Void> original) {
+        original.call(list, Collections.synchronizedMap(blockToInfos));
     }
 }
